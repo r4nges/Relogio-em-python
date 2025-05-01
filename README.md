7ee84c9c7c7fd096df8abd44a555ae62bf2237f3
rangel <rangelmacedo2006@gmail.com>
2025-05-01 14:49:22 -0300

    Comentários adicionados README/Código

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Relogio-em-python
 Relógio feito em python, objetivando o estudo da linguagem.
+
+Como funciona:
+Importa a biblioteca tkinter e chama de tk
+Importa uma de suas funções, de horário
+Cria a classe do relógio que serão feitas as configurações de fonte, posição na tela e alteração
+Define o horário da máquina e diz que a cada 1000ms, ele irá repetir o comando e atualizar
+Atribui a janela à biblioteca de GUI
--- relogio.py
+++ relogio.py
@@ -1,7 +1,11 @@
+# Import tkinter lib
 import tkinter as tk
 
+# Import datetime to get the current time
 import datetime
 
+# Create a class for the clock screen
+# This class will create a window with the current time
 class TelaRelogio:
     def __init__(self, master):
         self.tela = master
@@ -9,12 +13,14 @@ class TelaRelogio:
             self.tela, font=("Arial", 26), fg="black")
         self.hora.pack(pady=30, padx=30)
         self.alteracao()
-    
+    # This method will update the time every second
+    # It will get the current time and update the label
     def alteracao(self):
         now = datetime.datetime.now()
         self.hora['text'] = now.strftime("%H:%M:%S")
         self.tela.after(1000, self.alteracao)
 
+# This method will create the window
 janela = tk.Tk()
 TelaRelogio(janela)
 janela.mainloop()
